e20f3cab157716dfb58bc822da4005aa8692311d
Dexter Barrows <dexter@barrows.io>
2020-03-04 14:45:45 -0500

    attempt to fix typename issue with ponens c++ compiler

--- src/core/arma_helpers.h
+++ src/core/arma_helpers.h
@@ -5,8 +5,8 @@
 // hashing and equality functions for arma::uvec3.
 // Required to use arma::uvec3 as a key in std::unordered_map.
 template <>
-struct std::hash<arma::uvec3> {
-    std::size_t operator()(const arma::uvec3& k) const {
+struct std::hash<typename arma::uvec3> {
+    std::size_t operator()(const typename arma::uvec3& k) const {
         size_t res = 17;
         res *= 31 + std::hash<uint>()(k[0]);
         res *= 31 + std::hash<uint>()(k[1]);
@@ -15,8 +15,8 @@ struct std::hash<arma::uvec3> {
     }
 };
 template <>
-struct std::equal_to<arma::uvec3> {
-    bool operator()(const arma::uvec3& a, const arma::uvec3& b) const {
+struct std::equal_to<typename arma::uvec3> {
+    bool operator()(const typename arma::uvec3& a, const typename arma::uvec3& b) const {
         return a[0] == b[0] && a[1] == b[1] && a[2] == b[2];
     }
 };
