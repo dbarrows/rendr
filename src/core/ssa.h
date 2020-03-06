25f80f6cb7adef398ee40beaac54fcbfac1a2811
Dexter Barrows <dexter@barrows.io>
2020-03-05 19:07:39 -0500

    ssa: handle case when all species consumed before simulation end

--- src/core/ssa.h
+++ src/core/ssa.h
@@ -25,12 +25,23 @@ Rcpp::DataFrame ssa(const bondr::rnet& network, arma::vec y, double T, arma::vec
         sol.states.push_back(x);
     }
 
+    // keep track of case of early termination due to app reactants consumed
+    bool early_exit = false;
+
     while (t < T) {
         // setup
         for (uint i = 0; i < a.size(); i++)
             a[i] = (k.size() != 0 ? k[i] : 1.0)*network.reactions[i].propensity(x);
         arma::vec csum = cumsum(a);
         double asum = csum[csum.size() - 1];
+
+        // if all species consumed, report final state and finish
+        if (asum < 1e-15) {
+            sol.times.push_back(T);
+            sol.states.push_back(x);
+            early_exit = true;
+            break;
+        }
 
         // get reaction index `j`
         uint j = 0;
@@ -54,10 +65,11 @@ Rcpp::DataFrame ssa(const bondr::rnet& network, arma::vec y, double T, arma::vec
         }
     }
 
-    arma::vec x_interp = arma::round(((T - t_last)*x_last + (t - T)*x) / (t - t_last));
-
-    sol.times.push_back(T);
-    sol.states.push_back(x_interp);
+    if (!early_exit) {
+        sol.times.push_back(T);
+        arma::vec x_interp = arma::round(((T - t_last)*x_last + (t - T)*x) / (t - t_last));
+        sol.states.push_back(x_interp);
+    }
 
     return DataFrame(sol);
 }
