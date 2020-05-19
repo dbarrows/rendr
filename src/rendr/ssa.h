bd9c79efcd68e2e3cab57b48df2088c5b09a468e
Dexter Barrows <dexter@barrows.io>
2020-05-19 19:05:28 -0400

    internal refactoring

--- src/rendr/ssa.h
+++ src/rendr/ssa.h
@@ -7,6 +7,7 @@
 #include <arma_helpers.h>
 #include "ssa.h"
 #include "rsol.h"
+#include "ssa_helpers.h"
 
 namespace rendr {
 
@@ -36,26 +37,8 @@ inline Rcpp::DataFrame ssa(bondr::rnet network,
     }
 
     uint next_out = 0;
-    auto sol_push = [&sol, &next_out, y, all_out](double t, vec x){
-        // save everything
-        if (all_out) {
-            sol.t.push_back(t);
-            sol.u.push_back(x);
-        // length.out == 1: only save last state
-        } else if (sol.t.size() == 1 && sol.t[0] < t) {
-            sol.u[0] = interp(scale(sol.t[0], 0, t), y, x);
-        // length.out states: save first state
-        } else if (next_out == 0) {
-            sol.u[next_out++] = x;
-        // length.out states: save subsequent states
-        } else if (sol.t[next_out] <= t) {
-            double t0 = sol.t[next_out - 1];
-            vec x0 = sol.u[next_out - 1];
-            while (next_out < sol.t.size() && sol.t[next_out] < t) {
-                sol.u[next_out] = interp(scale(sol.t[next_out], t0, t), x0, x);
-                next_out++;
-            }
-        }
+    auto sol_push = [&sol, &next_out, y, all_out](double t, vec x) {
+        push(sol, t, x, y, all_out, next_out);
     };
 
     sol_push(t, x);
