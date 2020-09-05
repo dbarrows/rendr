c3bce03534aa55933155b216dbdf939af41ed0d9
Dexter Barrows <dexter@barrows.io>
2020-09-05 15:06:50 -0400

    add plotting support for multiple ssa trajectory solutions

--- R/rendr-package.R
+++ R/rendr-package.R
@@ -12,7 +12,7 @@
 #' @importFrom magrittr %>% %<>%
 #' @importFrom stringr str_c str_sub
 #' @importFrom tibble tibble as_tibble
-#' @importFrom dplyr rename select pull filter
+#' @importFrom dplyr rename select pull filter bind_rows group_by mutate summarise
 #' @importFrom tidyr pivot_longer
 #' @importFrom purrr walk
 #' @importFrom digest digest
@@ -21,9 +21,10 @@
 #' @importFrom crayon blurred blue silver
 #' @importFrom parallel detectCores mclapply
 #' @importFrom Rcpp sourceCpp
+#' @importFrom stats sd
 #' @useDynLib rendr, .registration = TRUE
 NULL
 
-## quiets concerns of R CMD check re:
-##  - the '.'s that appear in magrittr pipelines
-if(getRversion() >= '2.15.1') utils::globalVariables(c('.'))
+## quiets concerns of R CMD check re variables that appear in magrittr pipelines
+if(getRversion() >= '2.15.1')
+    utils::globalVariables(c('.', 'trajectory', 'lb', 'ub'))
--- R/rsol.R
+++ R/rsol.R
@@ -18,11 +18,41 @@ rsol <- function(sys, sol) {
 #' @export
 plot.rsol <- function(x, species = NULL, ...) {
     species <- if (is.null(species)) species(x) else species
-    x$sol %>%
+    if ('data.frame' %in% class(x$sol))
+        plot_rsol_single(x, species, ...)
+    else if (length(class(x$sol)) == 1 && 'list' == class(x$sol))
+        plot_rsol_multi(x, species, ...)
+    else
+        stop('.$sol must be a "list" or "data.frame"')
+}
+
+plot_rsol_single <- function(rsol, species) {
+    species <- if (is.null(species)) species(rsol) else species
+    rsol$sol %>%
         select(c(Time, species)) %>%
         pivot_longer(-Time, names_to = 'Species', values_to = 'Quantity') %>%
         ggplot(aes(Time, Quantity, colour = Species)) +
             geom_line()
+}
+
+plot_rsol_multi <- function(rsol, species) {
+    sols <- rsol$sol
+    n_trajectories <- length(sols)
+    n_times <- nrow(sols[[1]])
+    df <- 
+        sols %>%
+        bind_rows() %>%
+        select(Time, species) %>%
+        mutate(trajectory = sapply(1:n_trajectories, function(i) rep(i, n_times)) %>% c()) %>%
+        pivot_longer(-c(Time, trajectory), names_to = 'Species', values_to = 'Quantity') %>%
+        group_by(Time, Species) %>%
+        summarise(mean = mean(Quantity),
+                  lb = mean - 2*sd(Quantity),
+                  ub = mean + 2*sd(Quantity))
+    df %>%
+        ggplot(aes(Time, mean)) +
+            geom_line(aes(colour = Species)) +
+            geom_ribbon(aes(ymin = lb, ymax = ub, fill = Species), alpha = 0.25)
 }
 
 #' @export
