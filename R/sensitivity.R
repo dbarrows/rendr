b47f04a303626c03756c4e088252a32006fcbf60
Dexter Barrows <dexter@barrows.io>
2021-03-15 18:17:57 -0400

    sensitivity raw calculation working

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -1,7 +1,21 @@
+sensitivity <- function(rsys, length.out = 100) {
+    N <- length(species(rsys))
+    M <- length(rsys$network$reactions)
+    1:M %>% lapply(function(ci) {
+        y0 <- c(rsys$state, rep(0, N))
+        times <- seq(0, rsys$T, length.out = length.out)
+        dx <- sense_rhs(rsys$network, ci)
+        df <- ode(y0, times, dx) %>%
+            data.frame() %>%
+            as_tibble() %>%
+            set_names(c('Time', species(rsys), str_c('Sen', 1:N)))
+    })
+}
+
 sense_rhs <- function(network, ci) {
     function(t, y, parms, ...) {
         N <- length(species(network))
-        M <- length(net$reactions)
+        M <- length(network$reactions)
 
         x <- y[1:N]
         s <- y[-(1:N)]
@@ -17,7 +31,7 @@ sense_rhs <- function(network, ci) {
         ds <- 1:N %>% sapply(function(si) {
             1:M %>% sapply(function(ri) {
                 v <- vs[[ri]][si]
-                ac <- if (ri == ci ) as[[ri]](x)/ks[ri] else 0
+                ac <- if (ri == ci) as[[ri]](x)/ks[ri] else 0
                 ax <- 1:N %>% sapply(function(i) {
                         prop_px(net_ptr, x, ri, i)*s[i]
                     }) %>%
@@ -26,6 +40,7 @@ sense_rhs <- function(network, ci) {
             }) %>%
             sum()
         })
+        list(c(dx, ds))
     }
 }
 
